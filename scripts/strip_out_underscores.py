8a26ea3bb71e3924f98e60fe28b9aedf52eafe84
Chris Mackey <chris@mackeyarchitecture.com>
2018-12-10 15:40:52 -0500

    Updated insects to use the same color of black line

--- scripts/strip_out_underscores.py
+++ scripts/strip_out_underscores.py
@@ -4,6 +4,18 @@ import os
 
 current_folder = os.path.dirname(os.path.realpath(__file__))
 folder, scripts = os.path.split(current_folder)
+
+
+subfolders = ['png', 'emoji']
+
+for subdir in subfolders:
+    bug_directory = os.path.join(folder, 'icons_bugs', subdir)
+    for img_file in os.listdir(bug_directory):
+        new_img_file = img_file.replace('_', '')
+        original_file = os.path.join(bug_directory, img_file)
+        new_file = os.path.join(bug_directory, new_img_file)
+        os.rename(original_file, new_file)
+
 icons_folder = 'icons_components'
 plugins = ['ladybug', 'honeybee', 'dragonfly']
 subfolders = ['png', 'emoji']
